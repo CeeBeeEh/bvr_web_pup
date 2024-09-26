db781c5a08cbef6336ab3316601868aed44ac75b
chris <cblasko@sntechnologies.ca>
2024-09-26 15:03:14 -0400

    - added more cli args - changed incoming data processing to use multipart instead of a byte array - more robust return value processing - changed output to conform to another projects output style for compatibility

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,15 @@
-use std::io::Cursor;
+use std::io::Write;
 use std::sync::LazyLock;
 use axum::{
     routing::get,
     routing::post,
-    Router,
-    body::Bytes
+    Router
 };
-
+use axum_extra::extract::Multipart;
 use BvrDetect::{self, bvr_detect};
 use BvrDetect::bvr_data;
 use BvrDetect::bvr_data::{ProcessingType, DeviceType, ModelConfig};
 use clap::Parser;
-use image::ImageReader;
 use serde::Serialize;
 use tokio::sync::Mutex;
 use tokio::time::Instant;
@@ -23,6 +21,7 @@ static MODEL_CONFIG: LazyLock<Mutex<ModelConfig>> = LazyLock::new(|| Mutex::new(
         classes_path: String::new(),
         device_type: Default::default(),
         detector_type: Default::default(),
+        threshold: 0.4,
         width: 640,
         height: 640,
     }
@@ -50,8 +49,12 @@ struct Cli {
     device: String,
 
     /// Detection processing method to use. Either built-in onnxruntime or external python script. Options are [native, python]
-    #[arg(short='t', long, default_value = "native")]
+    #[arg(short='r', long, default_value = "native")]
     processing_type: String,
+
+    /// Override detection threshold
+    #[arg(short, long, default_value_t = 0.4)]
+    threshold: f32,
 
     /// The model's input width
     #[arg(short, long, default_value_t = 640)]
@@ -64,6 +67,14 @@ struct Cli {
     /// The port that should be used
     #[arg(short, long, default_value_t = 3000)]
     port: u16,
+}
+
+#[derive(Debug, Serialize)]
+struct Results {
+    success: bool,
+    inferenceMs: u32,
+    count: u32,
+    predictions: Vec<BasicDetectionResults>
 }
 
 #[derive(Debug, Serialize)]
@@ -90,6 +101,7 @@ async fn main() {
     model_config.onnx_path = cli.model_path.to_string();
     model_config.classes_path = cli.classes_path.to_string();
     model_config.ort_lib_path = cli.lib_path_ort.to_string();
+    model_config.threshold = if cli.threshold > 1.0 { cli.threshold / 100.0 } else { cli.threshold };
     model_config.width = cli.width;
     model_config.height = cli.height;
 
@@ -149,8 +161,8 @@ async fn root() -> String {
 ///
 /// # Returns
 /// * `String` - A JSON string representing the detected objects and their bounding boxes.
-async fn v1_detect(body: Bytes) -> String {
-    detect(body).await
+async fn v1_detect(multipart: Multipart) -> String {
+    detect(multipart).await
 }
 
 /// Object detection handler that processes the image in the request body and returns detection results.
@@ -160,71 +172,131 @@ async fn v1_detect(body: Bytes) -> String {
 ///
 /// # Returns
 /// * `String` - A JSON string containing a list of detected objects, their labels, confidence scores, and bounding boxes.
+async fn detect(mut multipart: Multipart) -> String {
+    let mut min_confidence = 0.0;
+    let mut image_data = Vec::new();
+    let mut file_found = false;
 
-async fn detect(body: Bytes) -> String {
     let now = Instant::now();
     let model_config = MODEL_CONFIG.lock().await;
     println!("\n{} - Running detection", chrono::offset::Local::now().format("%Y-%m-%d %H:%M:%S%.3f").to_string());
 
-    let byte_slice = &body[..];
-    let cursor = Cursor::new(byte_slice);
-    let image = ImageReader::new(cursor)
-        .with_guessed_format().unwrap().decode().unwrap();
+    // Iterate over the multipart fields
+    while let Some(mut field) = multipart.next_field().await.unwrap() {
+        let field_name = &field.name().unwrap_or_default().to_string();
 
-    let img_width = image.width() as i32;
-    let img_height = image.height() as i32;
+        if field_name == "min_confidence" {
+            let confidence_value = field.text().await.unwrap_or_else(|_| "0.0".to_string());
+            min_confidence = confidence_value.parse::<f32>().unwrap_or(0.0);
 
-    let bvr_image = bvr_data::BvrImage {
-        image,
-        img_width,
-        img_height,
-        conf_thres: 0.7,
-        iou_thres: 0.5,
-        augment: false,
-    };
+            // if provided threshold value is above 1.0, we assume it's a 0 - 100 int
+            // value and convert to f32 0.0 - 1.0
+            min_confidence = if min_confidence > 1.0 { min_confidence / 100.0 } else { min_confidence };
+        }
+        else if field_name == "image" {
+            let filename = field.file_name().unwrap_or("unknown").to_string();
 
-    if !bvr_detect::is_running().await.unwrap() {
-        let model_init = ModelConfig {
-            onnx_path: model_config.onnx_path.clone(),
-            ort_lib_path: model_config.ort_lib_path.clone(),
-            classes_path: model_config.classes_path.clone(),
-            device_type: model_config.device_type,
-            detector_type: Default::default(),
-            width: model_config.width,
-            height: model_config.height,
+            while let Ok(Some(chunk)) = field.chunk().await {
+                if let Err(e) = image_data.write_all(&chunk) {
+                    println!("Error writing file chunk: {:?}", e);
+                    return "Failed to parse image".to_string()
+                }
+            }
+
+            if let Err(e) = field.chunk().await {
+                // Handle the case where `field.chunk()` returns an error
+                println!("Error reading file chunk: {:?}", e);
+                return "Failed to read image".to_string()
+            }
+
+            file_found = true;
+            println!("Received file: {}", filename);
+        }
+    }
+
+    if file_found {
+        let image = image::load_from_memory(&image_data).map_err(|err| {
+            println!("Image decoding failed: {:?}", err);
+        }).unwrap();
+
+        let img_width = image.width() as i32;
+        let img_height = image.height() as i32;
+
+        let bvr_image = bvr_data::BvrImage {
+            image,
+            img_width,
+            img_height,
+            threshold: min_confidence,
+            augment: false,
         };
-        bvr_detect::init_detector(model_init, false).await;
-    }
 
-    let detections = bvr_detect::detect(bvr_image).await.expect("Error processing detection");
-    let mut detection_time = "(no results)".to_string();
+        if !bvr_detect::is_running().await.unwrap() {
+            let model_init = ModelConfig {
+                onnx_path: model_config.onnx_path.clone(),
+                ort_lib_path: model_config.ort_lib_path.clone(),
+                classes_path: model_config.classes_path.clone(),
+                device_type: model_config.device_type,
+                detector_type: Default::default(),
+                threshold: model_config.threshold,
+                width: model_config.width,
+                height: model_config.height,
+            };
+            bvr_detect::init_detector(model_init, false).await;
+        }
 
-    if detections.len() > 0 {
-        detection_time = (detections[0].last_inference_time / 1000).to_string() + "ms";
-    }
+        let detections = bvr_detect::detect(bvr_image).await.expect("Error processing detection");
+        let mut detection_time_str = "(no results)".to_string();
 
-    let mut results: Vec<BasicDetectionResults> = Vec::new();
+        if detections.is_empty() {
+            let empty_results = Results {
+                success: true,
+                inferenceMs: 0,
+                count: 0,
+                predictions: Vec::new(),
+            };
 
-    for detect in detections {
-        let bi_result = BasicDetectionResults {
-            label: detect.label,
-            confidence: detect.confidence,
-            x_min: detect.bbox.x1,
-            x_max: detect.bbox.x2,
-            y_min: detect.bbox.y1,
-            y_max: detect.bbox.y2,
+            println!("Found ZERO objects");
+            // Return basic info
+            return serde_json::to_string(&empty_results).unwrap()
         };
 
-        results.push(bi_result);
+        let detection_time = detections[0].last_inference_time / 1000;
+        if detections.len() > 0 {
+            detection_time_str = detection_time.to_string() + "ms";
+        }
+
+        let mut prediction_results: Vec<BasicDetectionResults> = Vec::new();
+
+        for detect in detections {
+            let bi_result = BasicDetectionResults {
+                label: detect.label,
+                confidence: detect.confidence,
+                x_min: detect.bbox.x1,
+                x_max: detect.bbox.x2,
+                y_min: detect.bbox.y1,
+                y_max: detect.bbox.y2,
+            };
+
+            prediction_results.push(bi_result);
+        }
+
+        let results = Results {
+            success: true,
+            inferenceMs: detection_time as u32,
+            count: prediction_results.len() as u32,
+            predictions: prediction_results,
+        };
+
+        let results_str = serde_json::to_string(&results).unwrap();
+
+        println!("Inference time: {}  |  Total process time: {}ms", detection_time_str, now.elapsed().as_millis());
+        println!("Found {} objects", results.count);
+        for det in results.predictions {
+            println!("{}", serde_json::to_string(&det).unwrap());
+        }
+
+        results_str
+    } else {
+        "No file found in the request".to_string()
     }
-
-    let results_str = serde_json::to_string(&results).unwrap();
-
-    println!("Inference time: {}  |  Total process time: {}ms", detection_time, now.elapsed().as_millis());
-    println!("Found {} objects", results.len());
-    for det in results {
-        println!("{}", serde_json::to_string(&det).unwrap());
-    }
-
-    results_str
 }
